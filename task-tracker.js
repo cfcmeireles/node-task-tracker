df529b9b3fa52a067d51534563fa4645ade833a0
Carina Meireles <carinacmeireles@gmail.com>
2024-09-07 23:18:45 +0100

    Add list, add and update commands

--- task-tracker.js
+++ task-tracker.js
@@ -2,44 +2,83 @@ const fs = require("fs");
 let tasks;
 let taskDescription;
 let taskId;
+let tasksClone;
 const args = process.argv.slice(2);
-const userInput = args[0];
+const command = args[0];
+const userInput = args[1];
+const updateTask = args[2];
 const currentDate = new Date().toLocaleString("en-US", {
   timeZoneName: "short",
 });
 
+// Commands
+if (command === "list") {
+  setImmediate(() => {
+    readTasksFile(() => {
+      console.log(taskDescription);
+    });
+  });
+}
+
+if (command === "add") {
+  setImmediate(() => {
+    readTasksFile(() => {
+      taskId = tasks[tasks.length - 1];
+      // Create new task object
+      const newTask = {
+        id: taskId.id + 1,
+        description: userInput,
+        status: "todo",
+        createdAt: currentDate,
+        updatedAt: "",
+      };
+      tasks.push(newTask);
+      tasksClone = tasks.slice();
+      writeTasksFile(tasksClone);
+      console.log(`Task added successfully (ID: ${taskId.id + 1})`);
+    });
+  });
+}
+
+if (command === "update" && updateTask) {
+  setImmediate(() => {
+    readTasksFile(() => {
+      taskId = tasks.map((task) => task.id);
+      for (var i = 0; i < taskId.length; i++) {
+        // Update selected task's description
+        if (taskId[i] === parseInt(userInput)) {
+          tasks[i].description = updateTask;
+          tasks[i].updatedAt = currentDate;
+        }
+      }
+      writeTasksFile(tasks);
+    });
+  });
+}
+
 // Read file
-fs.readFile("tasks.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("Error reading file:", err);
-    return;
-  }
-  try {
-    tasks = JSON.parse(data);
-    taskId = tasks[tasks.length - 1];
-    taskDescription = tasks.map((task) => task.description);
-  } catch (err) {
-    console.log("Error parsing JSON:", err);
-  }
+const readTasksFile = (callback) => {
+  fs.readFile("tasks.json", "utf8", (err, data) => {
+    if (err) {
+      console.log("Error reading file:", err);
+      return;
+    }
+    try {
+      tasks = JSON.parse(data);
+      taskDescription = tasks.map((task) => task.description);
+    } catch (err) {
+      console.log("Error parsing JSON:", err);
+    }
+    callback();
+  });
+};
 
-  // Create new task object
-  const newTask = {
-    id: taskId.id + 1,
-    description: userInput,
-    status: "todo",
-    createdAt: currentDate,
-    updatedAt: "",
-  };
-
-  console.log(taskDescription);
-  tasks.push(newTask);
-  const tasksClone = tasks.slice();
-
-  // Write tasks to file
+// Write tasks to file
+const writeTasksFile = (tasksClone) => {
   fs.writeFile("tasks.json", JSON.stringify(tasksClone), (err) => {
     if (err) {
       console.log("Error writing to file:", err);
       return;
     }
   });
-});
+};
